6b8654ae7d9bb33e3197863139c0aa2a56375130
spacechase0 <spacechase0.and.cat@gmail.com>
2019-01-16 17:04:26 -0500

    Initial commit

---
+++ Mod.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+using spacechase0.MiniModLoader.Api;
+using spacechase0.MiniModLoader.Api.Mods;
+using System.Reflection;
+
+namespace AlwaysMinimapPortraits
+{
+    public class Mod : IMod
+    {
+        public override void AfterModsLoaded()
+        {
+            var harmony = HarmonyInstance.Create("spacechase0.Portia.AlwaysMinimapPortraits");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Pathea.MapNs.Actor_IMap.cs
@@ -0,0 +1,42 @@
+﻿using Harmony;
+using Pathea.ActorNs;
+using Pathea.MapNs;
+using Pathea.ModuleNs;
+using Pathea.UISystemNs;
+using System.Reflection;
+using UnityEngine;
+
+namespace AlwaysMinimapPortraits
+{
+    [HarmonyPatch(typeof(Actor_IMap))]
+    [HarmonyPatch("Pathea.MapNs.IMap.GetIconInfo")]
+    public static class GetIconInfoPatch
+    {
+        private static FieldInfo overrideSpriteField = null;
+        
+        public static void Postfix(Actor_IMap __instance, ref PresetIcon ___iconInfo, ref Actor ___mActor)
+        {
+            if ( overrideSpriteField is null )
+            {
+                overrideSpriteField = typeof(PresetIcon).GetField("overrideSprite", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            }
+
+            var sprite = overrideSpriteField.GetValue(___iconInfo);
+
+            if ( __instance.GetIcon() == MapIcon.NPC )
+            {
+                ActorInfo actorInfo = Module<ActorMgr>.Self.GetActorInfo(___mActor.TmpltId);
+                if (!string.IsNullOrEmpty(actorInfo.miniHeadIcon))
+                {
+                    if (!actorInfo.miniHeadIcon.Contains("Dina")) // This doesn't exist for some reason
+                        ___iconInfo.SetOverrideSprite(UIUtils.GetSpriteByPath(actorInfo.miniHeadIcon), false, Vector2.zero);
+                }
+                else
+                {
+                    if (!actorInfo.icon.Contains("Dina")) // This doesn't exist for some reason
+                        ___iconInfo.SetOverrideSprite(UIUtils.GetSpriteByPath(actorInfo.icon), true, new Vector2(30f, 31.5f));
+                }
+            }
+        }
+    }
+}
